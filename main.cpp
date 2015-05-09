a23974c7c62ebf533e21be5dbbf3d3709c7ee239
Blagovest Taskov <taskov@gmail.com>
2015-05-09 16:45:51 +0300

    delete unused .js file

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <memory>
+
+#include "diffclock.h"
+
+#include "test_cache_miss.hpp"
+#include "test_pointer_alias/test_pointer_alias.h"
+#include "test_SoA.hpp"
+#include "test_inline/test_inline.h"
+#include "test_data_oriented_design.hpp"
+
+namespace PointerAlias {
+    void test();
+} //namespace PointerAlias
+
+int main(int argc, const char * argv[]) {
+    srand (0);
+    std::cout << "Starting tests ..." << std::endl;
+    DataOrientedDesign::test();
+    SoA::test();
+    Inline::test();
+    PointerAlias::test();
+    CacheMiss::test();
+    std::cout << "Tests completed."  << std::endl;
+    return 0;
+}
+
+
