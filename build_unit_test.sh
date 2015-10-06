9f5b57a4ab25c22192cc881e1255286991b82cd5
Martin Krastev <martin.krastev@chaosgroup.com>
2015-10-07 00:25:32 +0300

    touch up the unit-test build script

--- build_unit_test.sh
+++ build_unit_test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# use the CXX envvar to specify an alternative compiler (must be in PATH, otherwise specify with full path), like this:
+#
+# $ CXX=alt_compiler ./build_unit_test.sh
+
 if [[ -z ${CXX} ]]; then
 	CXX=clang++-3.7
 fi
@@ -12,6 +16,6 @@ SRC=(
 	test_pointer_alias/test_pointer_alias.cpp
 	test_pointer_alias/test_pointer_alias_impl.cpp
 )
-BUILD_CMD=${CXX}" "${SRC[@]}" -std=c++14 -I. -lpthread -O3 -fno-rtti -fstrict-aliasing -ffast-math -march=native -mtune=native"
+BUILD_CMD=${CXX}" "${SRC[@]}" -std=c++14 -I. -lpthread -O3 -DCXXOPTS_NO_RTTI -fno-rtti -fstrict-aliasing -ffast-math -march=native -mtune=native"
 echo $BUILD_CMD
 $BUILD_CMD
